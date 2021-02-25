655974dc128f1bc660bcf3c34f96afa5022d0749
Oğuz Erdoğan <ouzerdo@gmail.com>
2021-02-25 11:08:28 +0300

    cookie policy clicker and no image settings

--- Part 1 - Web Scraping/airbnb_parser.py
+++ Part 1 - Web Scraping/airbnb_parser.py
@@ -146,6 +146,12 @@ def extract_soup_js(listing_url, waiting_time=[20, 1]):
     except:
         pass
 
+    # click cookie policy
+    try:
+        driver.find_element_by_xpath("/html/body/div[6]/div/div/div[1]/section/footer/div[2]/button").click()
+    except:
+        pass
+
     # if the element is present - click on it
     if price_dropdown == 1:
         for i in range(10): # 10 attempts to scroll to the price button
